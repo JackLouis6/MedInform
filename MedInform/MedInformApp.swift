6c4862a1f2b559eaaceaddd7bba245dd0c66c9d9
Jack Louis <jbl5925@gmail.com>
2022-03-31 10:31:53 -0500

    Initial Commit

---
+++ MedInform/MedInformApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedInformApp.swift
+//  MedInform
+//
+//  Created by Student on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct MedInformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
