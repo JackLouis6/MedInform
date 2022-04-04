b499bfa2e10eebf21fd4da6a2bfc77f355680a54
Jack Louis <jbl5925@gmail.com>
2022-04-04 10:26:47 -0500

    Learned and wrote down a simple reminder function

--- MedInform/ContentView.swift
+++ MedInform/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
         Text("Weclome to MedInform")
             .padding()
+        
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("Notification Set")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Take my medication"
+                content.subtitle = "Advil"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+                
+            }
+        }
     }
 }
 
