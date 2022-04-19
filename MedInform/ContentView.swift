57b413fa500b1867b85ee98b48ba91a5e2d55677
Jack Louis <jbl5925@gmail.com>
2022-04-19 10:58:54 -0500

    Transferred pizza finder code for editing

--- MedInform/ContentView.swift
+++ MedInform/ContentView.swift
@@ -6,20 +6,63 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var places = [Place]()
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.159332,
+            longitude: -88.138847),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
+    
     var body: some View {
-        Text("Welcome to MedInform. This app will be used to benefit your medical needs. You will get reminders to take your medications and what medications to take.")
-            .font(.body)
-            .fontWeight(.heavy)
-            .foregroundColor(Color.black)
-            .multilineTextAlignment(.center)
-            .padding(2.0)
-        
-        NavigationLink(destination: Reminder(), label: {
-            Text("Reminder")
+        Map(
+            coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.annotation.coordinate) {
+                Marker(mapItem: place.mapItem)
+            }
+        }
+        .onAppear(perform: {
+            performSearch(item: "Pizza")
         })
-        
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
+        }
+    }
+}
+
+struct Marker: View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("pizza")
+            })
+        }
     }
 }
 
@@ -27,4 +70,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
 }
