a56dbdaf02cbc3e0753d7e2c07b938df2a805509
Jack Louis <jbl5925@gmail.com>
2022-04-26 10:40:25 -0500

    ContentView Commit

--- MedInform/ContentView.swift
+++ MedInform/ContentView.swift
@@ -6,75 +6,20 @@
 //
 
 import SwiftUI
-import MapKit
+
 
 struct ContentView: View {
-    @StateObject var locationManager = LocationManager()
-    @State private var userTrackingMode: MapUserTrackingMode = .follow
-    @State private var places = [Place]()
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 42.159332,
-            longitude: -88.138847),
-        span: MKCoordinateSpan(
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05)
-    )
-    
     var body: some View {
-        Map(
-            coordinateRegion: $region,
-            interactionModes: .all,
-            showsUserLocation: true,
-            userTrackingMode: $userTrackingMode,
-            annotationItems: places) { place in
-            MapAnnotation(coordinate: place.annotation.coordinate) {
-                Marker(mapItem: place.mapItem)
-            }
-        }
-        .onAppear(perform: {
-            performSearch(item: "Urgrent Care")
-            performSearch(item: "Emergency Room")
-        })
-    }
-    
-    func performSearch(item: String) {
-        let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = item
-        searchRequest.region = region
-        let search = MKLocalSearch(request: searchRequest)
-        search.start { response, error in
-            if let response = response {
-                for mapItem in response.mapItems {
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = mapItem.placemark.coordinate
-                    annotation.title = mapItem.name
-                    places.append(Place(annotation: annotation, mapItem: mapItem))
-                }
-            }
-        }
-    }
-}
-
-struct Marker: View {
-    var mapItem: MKMapItem
-    var body: some View {
-        if let url = mapItem.url {
-            Link(destination: url, label: {
-                Image("redcross")
+        NavigationView {
+            NavigationLink(destination: MapView(), label: {
+                Text("Map")
             })
+            
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-struct Place: Identifiable {
-    let id = UUID()
-    let annotation: MKPointAnnotation
-    let mapItem: MKMapItem
 }
---
+++ MedInform/MapView.swift
@@ -0,0 +1,80 @@
+//
+//  MapView.swift
+//  MedInform
+//
+//  Created by Student on 4/26/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @StateObject var locationManager = LocationManager()
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var places = [Place]()
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.159332,
+            longitude: -88.138847),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
+    
+    var body: some View {
+        Map(
+            coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.annotation.coordinate) {
+                Marker(mapItem: place.mapItem)
+            }
+        }
+        .onAppear(perform: {
+            performSearch(item: "Urgrent Care")
+            performSearch(item: "Emergency Room")
+        })
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
+        }
+    }
+}
+
+struct Marker: View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("redcross")
+            })
+        }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
+}
